4b9c6fa871130ec805881facb75fafc7f72a172c
zastix fanboy <102484505+l2vy7@users.noreply.github.com>
2022-06-12 22:06:57 -0400

    Create zastixsecure.js

---
+++ scripts/zastixsecure.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         anti xotic stuff
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  no bad things xotic
+// @author       You
+// @match        https://betastar.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=betastar.org
+// @grant        none
+// ==/UserScript==
+
+socket.off('clear')
+socket.off('run')
+socket.off('announcement')
